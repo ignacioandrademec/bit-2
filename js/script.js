2985b85e7e77fa49f40a7aeff34a5cb1284c8d27
Jose Andrade <ignacio.andrademec@gmail.com>
2025-06-03 11:40:25 -0500

    adelantos entrega 3

--- js/script.js
+++ js/script.js
@@ -1,5 +1,40 @@
 "use strict"
 
+// FORMULARIO SENCILLO
+
+const $contactForm2 = document.getElementById("contactForm2");
+$contactForm2.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const name = $contactForm2.name.value;
+    const email = $contactForm2.email.value;
+    const message = $contactForm2.message.value;
+
+    console.log(name);
+    console.log(email);
+    console.log(message);
+    
+    const result = `Apreciado ${name}.
+    
+Gracias por diligenciar la información.
+
+Su correo ${email}, fue registrado satisfactoriamente,
+    
+Uno de nuestros asesores del área académica atenderá su mensaje: "${message}", lo más pronto posible, que tenga un excelente día.
+    
+Cordialmente, 
+    
+BOGOTÁ INSTITUTE OF TECHNOLOGY
+`;
+
+    alert(result);
+
+    $contactForm2.reset ();
+
+});
+
+
+
+
 // const $l1 = document.getElementById("l1")
 
 
