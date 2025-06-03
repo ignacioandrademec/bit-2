b4bcf94e797af157ba4018d724478345878ecd47
Jose Andrade <ignacio.andrademec@gmail.com>
2025-06-03 01:09:03 -0500

    avance bit-2

--- js/script.js
+++ js/script.js
@@ -1,2 +1,26 @@
 "use strict"
 
+// const $l1 = document.getElementById("l1")
+
+
+
+// fetch("./file.json")
+//     .then((res)=> res.json())
+//     .then((info)=>{
+//     // console.log(data[0].student);
+//     for (let i = 0; i < info.length; i++) {
+//         console.log(info[i].student)
+
+//     let listado = "";
+
+//     for (let i = 0; i < info.length; i++) {
+//         listado += `<li>${info[i].student}</li>`
+//     }
+
+//     $l1.innerHTML = listado;
+
+//     }
+// }).catch((err)=>{
+//     console.log("error: ", err);
+// });
+
